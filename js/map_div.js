312f8ccb8778fe4573cdb979c58f43b571faf379
Arnaud Poletto <arnaud.poletto@epfl.ch>
2023-04-04 15:44:53 +0200

    remove raise

--- js/map_div.js
+++ js/map_div.js
@@ -154,8 +154,7 @@ class DivisionsMap {
                 .attr("cy", d => this.projection(d[1])[1])
                 .attr("r", 2)
                 .attr("fill", "black")
-                .style("pointer-events", "none")
-                .raise();
+                .style("pointer-events", "none");
 
             this.layer_2.selectAll("text")
                 .data(data)
@@ -169,8 +168,7 @@ class DivisionsMap {
                 .style("fill", "black")
                 .style("text-anchor", "middle")
                 .style("pointer-events", "none")
-                .style("font-family", "sans-serif")
-                .raise();
+                .style("font-family", "sans-serif");
         });
     }
 
